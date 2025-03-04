38db24d709f76a7ef0448b295af310797c8e2ccb
xaratustrah <xaratustrah@users.noreply.github.com>
2025-03-04 17:43:35 +0100

    .

--- sdr2zmq/sdr2zmq.cpp
+++ sdr2zmq/sdr2zmq.cpp
@@ -1,25 +1,14 @@
-//
-// SDR2ZMQ
-// Inspired by the original soapy example in the GR4
-// https://github.com/fair-acc/gnuradio4/blob/main/blocks/soapy/src/soapy_example.cpp
-//
-// (2025) xaratustrah@github
-//
-
 #include <boost/ut.hpp>
 #include <spdlog/spdlog.h>
-
-#include <algorithm>
-#include <filesystem>
-#include <fmt/format.h>
-#include <optional>
 #include <string>
-#include <tuple>
 
 #include <gnuradio-4.0/Scheduler.hpp>
 #include <gnuradio-4.0/soapy/Soapy.hpp>
 #include <gnuradio-4.0/zeromq/Zmq.hpp>
 #include <gnuradio-4.0/testing/NullSources.hpp>
+
+// Define a constant buffer size
+const std::size_t BUFFER_SIZE = 2048;
 
 gr::Graph createGraph(std::string zmqPubAddr1, std::string zmqPubAddr2, float sampleRate, double rxCenterFrequency, double bandwidth, double rxGains) {
     using namespace boost::ut;
@@ -30,6 +19,7 @@ gr::Graph createGraph(std::string zmqPubAddr1, std::string zmqPubAddr2, float sa
     Graph flow;
     using TDataType = std::complex<float>;
 
+    // Create source block with specified buffer size
     auto& source = flow.emplaceBlock<SoapyBlock<TDataType, 2UZ>>({
         {"device", "lime"},                                                                 //
         {"sample_rate", sampleRate},                                                        //
@@ -37,7 +27,8 @@ gr::Graph createGraph(std::string zmqPubAddr1, std::string zmqPubAddr2, float sa
         {"rx_antennae", std::vector<std::string>{"LNAW", "LNAW"}},                          //
         {"rx_center_frequency", std::vector<double>{rxCenterFrequency, rxCenterFrequency}}, //
         {"rx_bandwidth", std::vector<double>{bandwidth, bandwidth}},                        //
-        {"rx_gains", std::vector<double>{rxGains, rxGains}},
+        {"rx_gains", std::vector<double>{rxGains, rxGains}},                                //
+        {"buffer_size", BUFFER_SIZE}                                                        // Specify buffer size
     });
     spdlog::info("set parameter: sample_rate: {} SP/s, rx_center_frequency: {} Hz, rx_bandwidth: {} Hz, rx_gains: {} dB", //
                  sampleRate, rxCenterFrequency, bandwidth, rxGains);
@@ -45,20 +36,28 @@ gr::Graph createGraph(std::string zmqPubAddr1, std::string zmqPubAddr2, float sa
     if (zmqPubAddr1.contains("null")) {
         spdlog::info("write channel0 to NullSink");
         auto& zmqSink1 = flow.emplaceBlock<testing::NullSink<TDataType>>();
+        zmqSink1.set_output_multiple(BUFFER_SIZE); // Set buffer size for NullSink
         expect(eq(gr::ConnectionResult::SUCCESS, flow.connect<"out0">(source).to<"in">(zmqSink1))) << "error connecting NullSink1";
     } else {
         spdlog::info("write to ZMQ PUB address: {}", zmqPubAddr1);
-        auto& zmqSink1 = flow.emplaceBlock<ZmqPubSink<TDataType>>({{"addr", zmqPubAddr1}});
+        auto& zmqSink1 = flow.emplaceBlock<ZmqPubSink<TDataType>>({
+            {"addr", zmqPubAddr1}, 
+            {"buffer_size", BUFFER_SIZE} // Specify buffer size for ZMQ sink
+        });
         expect(eq(gr::ConnectionResult::SUCCESS, flow.connect<"out0">(source).to<"in">(zmqSink1))) << "error connecting ZmqPubSink1";
     }
 
     if (zmqPubAddr2.contains("null")) {
         spdlog::info("write channel1 to NullSink");
         auto& zmqSink2 = flow.emplaceBlock<testing::NullSink<TDataType>>();
+        zmqSink2.set_output_multiple(BUFFER_SIZE); // Set buffer size for NullSink
         expect(eq(gr::ConnectionResult::SUCCESS, flow.connect<"out1">(source).to<"in">(zmqSink2))) << "error connecting NullSink2";
     } else {
         spdlog::info("write to ZMQ PUB address: {}", zmqPubAddr2);
-        auto& zmqSink2 = flow.emplaceBlock<ZmqPubSink<TDataType>>({{"addr", zmqPubAddr2}});
+        auto& zmqSink2 = flow.emplaceBlock<ZmqPubSink<TDataType>>({
+            {"addr", zmqPubAddr2}, 
+            {"buffer_size", BUFFER_SIZE} // Specify buffer size for ZMQ sink
+        });
         expect(eq(gr::ConnectionResult::SUCCESS, flow.connect<"out1">(source).to<"in">(zmqSink2))) << "error connecting ZmqPubSink2";
     }
 
@@ -66,7 +65,7 @@ gr::Graph createGraph(std::string zmqPubAddr1, std::string zmqPubAddr2, float sa
 }
 
 int main(int argc, char* argv[]) {
-    spdlog::info("Starting the aquisition...");
+    spdlog::info("Starting the acquisition...");
 
     try {
         using namespace boost::ut;
@@ -86,7 +85,7 @@ int main(int argc, char* argv[]) {
         gr::Scheduler scheduler;
         scheduler.run(graph);
 
-        spdlog::info("Aquisition finished successfully.");
+        spdlog::info("Acquisition finished successfully.");
     } catch (const std::exception& e) {
         spdlog::error("An error occurred: {}", e.what());
         return 1;
